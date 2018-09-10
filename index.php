fcb5965180b83c0340a6f183af183b699cfff9a4
Haulund <steffen.haulund@gmail.com>
2018-09-10 10:41:38 +0200

    basic grid opsætning

--- index.php
+++ index.php
@@ -3,13 +3,38 @@
         <head>
                 <meta charset="utf-8">
                  <link rel="stylesheet" type="text/css" href="css/styles.css">
+                 <link href="https://fonts.googleapis.com/css?family=Catamaran:400,700" rel="stylesheet">
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                  <meta name="Robots" content="Noindex,nofollow">
                 <title></title>
         </head>
         <body>
-          <div class="app-container">
-                  <p>test</p>
 
-          </div>
+                        <div class="app-container">
+                                <div class="topleft">
+                                        <a href="#"><img src="img/arkivikon.svg" alt=""></a>
+                                </div>
+                                <div class="topmid">
+
+                                </div>
+                                <div class="topright">
+                                        <a href="#"><img src="img/lukikon.svg" alt=""></a>
+                                </div>
+
+                                <div class="mid">
+                                        <a href="#"><img src="img/samtaleikon.svg" alt=""></a>
+                                        <p>Find en kollega</p>
+                                </div>
+                                <div class="bottomleft">
+                                        <a href="#"><img src="img/sendsmsikon.svg" alt=""></a>
+                                </div>
+                                <div class="bottommid">
+
+                                </div>
+                                <div class="bottomright">
+                                        <a href="#"><img src="img/pilfrem.svg" alt=""></a>
+                                </div>
+                        </div>
 
         </body>
 </html>
