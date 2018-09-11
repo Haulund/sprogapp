37d9b67f655eb6527a15ca570b4ac56091597723
Haulund <steffen.haulund@gmail.com>
2018-09-11 13:38:47 +0200

    Tak for idag Steffen 1

---
+++ forside.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+        <head>
+                <meta charset="utf-8">
+                 <link rel="stylesheet" type="text/css" href="css/styles.css">
+                 <link href="https://fonts.googleapis.com/css?family=Catamaran:400,700" rel="stylesheet">
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                  <meta name="Robots" content="Noindex,nofollow">
+                <title></title>
+        </head>
+        <body>
+                <div class="app-container">
+                        <div class="score">
+                                <p>15</p>
+                                <p>ord</p>
+                        </div>
+                        <div class="funktioner">
+                                <label class="addword"></label>
+                                <label class="ordbog"></label>
+                        </div>
+                        <div class="spil">
+                                <div class="overskrift">
+
+                                </div>
+                                <div class="spil1">
+
+                                </div>
+                                <div class="spil2">
+
+                                </div>
+                                <div class="spil3">
+
+                                </div>
+                        </div>
+
+
+                </div>
+        </body>
+</html>
